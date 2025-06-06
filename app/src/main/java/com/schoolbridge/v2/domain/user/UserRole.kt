82125194f6f6c0355f5c73e18bc4287feafa3dd0
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-06 06:53:09 +0200

    Define UserRole enum
    This commit introduces the `UserRole` enum, which defines the different roles a user can have within the application.
    
    The defined roles are:
    - `STUDENT`
    - `TEACHER`
    - `PARENT`
    - `SCHOOL_ADMIN`
    - `GUEST` (for unauthenticated access)

---
+++ app/src/main/java/com/schoolbridge/v2/domain/user/UserRole.kt
@@ -0,0 +1,9 @@
+package com.schoolbridge.v2.domain.user
+
+enum class UserRole {
+    STUDENT,
+    TEACHER,
+    PARENT,
+    SCHOOL_ADMIN,
+    GUEST // If you have unauthenticated guest access
+}
