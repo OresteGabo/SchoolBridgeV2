d6dadb8198f52a8ecfe140c73697c7da9d088c2d
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-12 21:36:13 +0200

    Create FinanceScreen.kt
    This commit introduces a new Kotlin file `FinanceScreen.kt` within the `com.schoolbridge.v2.ui.finance` package.
    
    The file currently contains a composable function `FinanceScreen` that displays the text "FinanceScreen".

---
+++ app/src/main/java/com/schoolbridge/v2/ui/finance/FinanceScreen.kt
@@ -0,0 +1,10 @@
+package com.schoolbridge.v2.ui.finance
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FinanceScreen(modifier: Modifier = Modifier) {
+    Text("FinanceScreen")
+}
