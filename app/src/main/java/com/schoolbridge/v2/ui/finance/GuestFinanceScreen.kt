ede70dabafb6fe4f83527af28fcd9752444635b8
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-12 21:35:41 +0200

    Create GuestFinanceScreen.kt
    This commit introduces the `GuestFinanceScreen.kt` file within the `com.schoolbridge.v2.ui.finance` package.
    
    It defines a Composable function `GuestFinanceScreen` which currently displays the text "GuestFinanceScreen".

--- app/src/main/java/com/schoolbridge/v2/ui/finance/GuestFinanceScreen.kt
+++ app/src/main/java/com/schoolbridge/v2/ui/finance/GuestFinanceScreen.kt
@@ -1,4 +1,10 @@
 package com.schoolbridge.v2.ui.finance
 
-class GuestFinanceScreen {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GuestFinanceScreen(modifier: Modifier = Modifier) {
+    Text("GuestFinanceScreen")
 }
