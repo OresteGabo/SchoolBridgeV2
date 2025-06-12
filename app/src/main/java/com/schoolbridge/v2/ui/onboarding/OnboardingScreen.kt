ebd7cd92fad7e67a89d521f80d958641f7df4ae5
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-12 21:36:42 +0200

    Create OnboardingScreen.kt
    This commit introduces the `OnboardingScreen.kt` file within the `com.schoolbridge.v2.ui.onboarding` package.
    
    The file contains a basic Composable function `OnboardingScreen` that displays the text "Onboarding screen".

---
+++ app/src/main/java/com/schoolbridge/v2/ui/onboarding/OnboardingScreen.kt
@@ -0,0 +1,10 @@
+package com.schoolbridge.v2.ui.onboarding
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    Text("Onboarding screen")
+}
