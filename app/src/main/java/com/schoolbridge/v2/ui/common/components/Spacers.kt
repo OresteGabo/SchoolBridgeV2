8d26c522d2e947e4fef049f6ee9bb357a78b308c
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-13 06:37:16 +0200

    Implement Spacer components
    This commit introduces a set of composable functions for creating vertical spacers of predefined heights.
    
    The following spacer components have been added:
    - `SpacerXS()`: 4.dp height
    - `SpacerS()`: 8.dp height
    - `SpacerM()`: 16.dp height
    - `SpacerL()`: 24.dp height
    - `SpacerXL()`: 32.dp height
    - `SpacerXXL()`: 48.dp height
    
    These components can be used to easily add consistent vertical spacing within Jetpack Compose layouts.

--- app/src/main/java/com/schoolbridge/v2/ui/common/components/Spacers.kt
+++ app/src/main/java/com/schoolbridge/v2/ui/common/components/Spacers.kt
@@ -1,4 +1,15 @@
 package com.schoolbridge.v2.ui.common.components
 
-class Spacers {
-}
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable fun SpacerXS() = Spacer(Modifier.height(4.dp))
+@Composable fun SpacerS() = Spacer(Modifier.height(8.dp))
+@Composable fun SpacerM() = Spacer(Modifier.height(16.dp))
+@Composable fun SpacerL() = Spacer(Modifier.height(24.dp))
+@Composable fun SpacerXL() = Spacer(Modifier.height(32.dp))
+@Composable fun SpacerXXL() = Spacer(Modifier.height(48.dp))
