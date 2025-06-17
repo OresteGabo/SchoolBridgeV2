4dfe88a597c8c6adff3da1a4daf1b8a9918cc363
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-18 00:42:34 +0200

    Introduce StudentListSection composable
    This commit adds a new composable function `StudentListSection` to `StudentListSection.kt`.
    
    This composable displays a horizontally scrollable list of `StudentCard` components.
    - It takes a list of `CurrentUser.LinkedStudent` and an optional `Modifier` as parameters.
    - If the list of students is not null or empty, it displays a subheader "📚 Your children" and then the `LazyRow` of `StudentCard`s.
    - Each `StudentCard` is spaced by 12.dp.

---
+++ app/src/main/java/com/schoolbridge/v2/ui/home/student/StudentListSection.kt
@@ -0,0 +1,39 @@
+package com.schoolbridge.v2.ui.home.student
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.schoolbridge.v2.R
+import com.schoolbridge.v2.domain.user.CurrentUser
+import com.schoolbridge.v2.localization.t
+import com.schoolbridge.v2.ui.common.components.AppSubHeader
+import com.schoolbridge.v2.ui.common.components.SpacerS
+
+/**
+ * Displays horizontally scrollable student cards.
+ *
+ * @param students List of linked students, nullable.
+ * @param modifier Modifier applied to the section.
+ */
+@Composable
+fun StudentListSection(
+    students: List<CurrentUser.LinkedStudent>?,
+    modifier: Modifier = Modifier
+) {
+    if (!students.isNullOrEmpty()) {
+        Row(modifier = modifier.fillMaxWidth()) {
+            AppSubHeader("📚 " + t(R.string.your_children))
+        }
+        SpacerS()
+        LazyRow(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+            itemsIndexed(students) { _, student ->
+                StudentCard(student = student)
+            }
+        }
+    }
+}
