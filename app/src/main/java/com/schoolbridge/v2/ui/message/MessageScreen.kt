9c7acc7890baa5f12f5e7509dcbe775f74e94ea7
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-18 05:38:37 +0200

    Initialize MessageScreen with basic structure
    This commit introduces the `MessageScreen` composable with a basic layout:
    
    - A `Scaffold` provides the main structure.
    - A `TopAppBar` is included with a title "Message" and a back arrow icon.
    - A `CustomBottomNavBar` is integrated for navigation.
    - The `userSessionManager`, `currentScreen`, and `onTabSelected` parameters are accepted to manage session data and navigation state.
    - Placeholder for main content within the `Scaffold`.

--- app/src/main/java/com/schoolbridge/v2/ui/message/MessageScreen.kt
+++ app/src/main/java/com/schoolbridge/v2/ui/message/MessageScreen.kt
@@ -1,4 +1,66 @@
 package com.schoolbridge.v2.ui.message
 
-class MessageScreen {
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.schoolbridge.v2.R
+import com.schoolbridge.v2.components.CustomBottomNavBar
+import com.schoolbridge.v2.data.session.UserSessionManager
+import com.schoolbridge.v2.domain.messaging.Alert
+import com.schoolbridge.v2.localization.t
+import com.schoolbridge.v2.ui.home.alert.AlertDetailsBottomSheetContent
+import com.schoolbridge.v2.ui.navigation.MainAppScreen
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MessageScreen(
+    userSessionManager: UserSessionManager,
+    currentScreen: MainAppScreen,
+    onTabSelected: (MainAppScreen) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    // Main Scaffold outside of ModalBottomSheet to avoid gesture blocking
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Message") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = t(R.string.back))
+                    }
+                }
+            )
+        },
+        bottomBar = { CustomBottomNavBar(
+            currentScreen = currentScreen,
+            onTabSelected = onTabSelected
+        ) },
+        modifier = modifier
+    ) { paddingValues ->
+
+        // Main content
+
+
+        // Render bottom sheet only when alert is selected
+
+    }
 }
