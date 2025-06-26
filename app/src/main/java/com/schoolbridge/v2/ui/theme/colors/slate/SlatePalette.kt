33ad6479f4765f3f5332243f4f722b7efc3c51a3
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-26 09:01:48 +0200

    Rename GreenPalette to SlatePalette and update surfaceBrightLight
    This commit renames `GreenPalette.kt` to `SlatePalette.kt` and changes the `GreenPalette` object to `SlatePalette`.
    
    Additionally, the `surfaceBrightLight` color for the light theme is updated from `com.schoolbridge.v2.ui.theme.colors.red.surfaceBrightLight` to `com.schoolbridge.v2.ui.theme.colors.teal.surfaceBrightLight`.

--- app/src/main/java/com/schoolbridge/v2/ui/theme/colors/slate/SlatePalette.kt
+++ app/src/main/java/com/schoolbridge/v2/ui/theme/colors/slate/SlatePalette.kt
@@ -1,9 +1,8 @@
-package com.schoolbridge.v2.ui.theme.colors.green
+package com.schoolbridge.v2.ui.theme.colors.slate
 
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.ColorScheme
-import com.schoolbridge.v2.ui.theme.Contrast
 import com.schoolbridge.v2.ui.theme.LightDark
 import com.schoolbridge.v2.ui.theme.PaletteVariants
 /* -------------------------------------------------------------
@@ -40,7 +39,7 @@ private val normalLight: ColorScheme = lightColorScheme(
     inverseOnSurface = inverseOnSurfaceLight,
     inversePrimary = inversePrimaryLight,
     surfaceDim = surfaceDimLight,
-    surfaceBright = com.schoolbridge.v2.ui.theme.colors.red.surfaceBrightLight,
+    surfaceBright = com.schoolbridge.v2.ui.theme.colors.teal.surfaceBrightLight,
     surfaceContainerLowest = surfaceContainerLowestLight,
     surfaceContainerLow = surfaceContainerLowLight,
     surfaceContainer = surfaceContainerLight,
@@ -241,7 +240,7 @@ private val highDark: ColorScheme = darkColorScheme(
 /* -------------------------------------------------------------
    2.  Expose ONE object that bundles the six schemes.
    ------------------------------------------------------------- */
-val GreenPalette = PaletteVariants(
+val SlatePalette = PaletteVariants(
     normal = LightDark(normalLight,  normalDark ),
     medium = LightDark(mediumLight,  mediumDark ),
     high   = LightDark(highLight,    highDark   )
