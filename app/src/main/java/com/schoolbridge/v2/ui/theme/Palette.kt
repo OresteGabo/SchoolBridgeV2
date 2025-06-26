8f733f773a054cbb52497c1cc3d34b96e3c50c69
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-06-26 09:24:00 +0200

    Add Material theme to Palette
    This commit introduces a new `MATERIAL` theme option to the `Palette` enum.
    
    - The `MATERIAL` theme uses `MaterialPalette`.

--- app/src/main/java/com/schoolbridge/v2/ui/theme/Palette.kt
+++ app/src/main/java/com/schoolbridge/v2/ui/theme/Palette.kt
@@ -2,6 +2,7 @@ package com.schoolbridge.v2.ui.theme
 
 import com.schoolbridge.v2.ui.theme.colors.golden.GoldenPalette
 import com.schoolbridge.v2.ui.theme.colors.indigo.IndigoPalette
+import com.schoolbridge.v2.ui.theme.colors.material.MaterialPalette
 import com.schoolbridge.v2.ui.theme.colors.slate.SlatePalette
 import com.schoolbridge.v2.ui.theme.colors.teal.TealPalette
 
@@ -10,5 +11,6 @@ enum class AppPalette(val variants: PaletteVariants) {
     GOLDEN(GoldenPalette),
     BLUE  (IndigoPalette),
     GREEN (SlatePalette),
-    RED   (TealPalette);
+    RED   (TealPalette),
+    MATERIAL(MaterialPalette);
 }
