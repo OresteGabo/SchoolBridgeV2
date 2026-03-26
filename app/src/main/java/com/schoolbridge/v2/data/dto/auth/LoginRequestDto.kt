2a76fffb9decfe39598b59f8bf834c8762a881c1
oreste <muhirwa.g.oreste@gmail.com>
2026-03-26 18:18:57 +0100

    Remove unused Gson `@SerializedName` annotations from `LoginRequestDto`.

--- app/src/main/java/com/schoolbridge/v2/data/dto/auth/LoginRequestDto.kt
+++ app/src/main/java/com/schoolbridge/v2/data/dto/auth/LoginRequestDto.kt
@@ -1,6 +1,5 @@
 package com.schoolbridge.v2.data.dto.auth
 
-import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 /**
@@ -21,8 +20,6 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class LoginRequestDto(
-    //@SerializedName("usernameOrEmail")
     val username: String,
-    //@SerializedName("password")
     val password: String
 )
