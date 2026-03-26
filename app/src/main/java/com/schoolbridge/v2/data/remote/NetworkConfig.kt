b3f5fcba4116554d031c62ec836fe993a3a9ba5f
oreste <muhirwa.g.oreste@gmail.com>
2026-03-26 18:21:54 +0100

    Add `NetworkConfig` object with `apiBaseUrl` for remote API configuration

---
+++ app/src/main/java/com/schoolbridge/v2/data/remote/NetworkConfig.kt
@@ -0,0 +1,9 @@
+package com.schoolbridge.v2.data.remote
+
+object NetworkConfig {
+    /**
+     * Set this to the laptop IP address that your Android tablet can reach.
+     * 127.0.0.1 only works from the same machine, not from a physical device.
+     */
+    const val apiBaseUrl: String = "http://172.20.10.2:8080"
+}
