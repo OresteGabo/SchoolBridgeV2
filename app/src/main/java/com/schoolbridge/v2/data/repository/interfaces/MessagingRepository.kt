37eed4f3963d3fa27459d577a18aae3a71ba7bfb
oreste <muhirwa.g.oreste@gmail.com>
2026-03-27 21:02:10 +0100

    Add `sendMessage` and `markMessageAsRead` methods to `MessagingRepository` interface

--- app/src/main/java/com/schoolbridge/v2/data/repository/interfaces/MessagingRepository.kt
+++ app/src/main/java/com/schoolbridge/v2/data/repository/interfaces/MessagingRepository.kt
@@ -4,4 +4,6 @@ import com.schoolbridge.v2.data.dto.message.MobileMessageThreadDto
 
 interface MessagingRepository {
     suspend fun getMessageThreads(): List<MobileMessageThreadDto>
+    suspend fun sendMessage(conversationId: Long, senderId: Long, content: String)
+    suspend fun markMessageAsRead(messageId: Long, userId: Long)
 }
