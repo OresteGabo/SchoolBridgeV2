2239d5291e0b625f8d52c1e611ef439dc7d6cbdb
oreste <muhirwa.g.oreste@gmail.com>
2026-03-27 23:41:02 +0100

    Remove `TodayScheduleRepository` interface as it is no longer used.

--- app/src/main/java/com/schoolbridge/v2/data/repository/interfaces/TodayScheduleRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.schoolbridge.v2.data.repository.interfaces
-
-import com.schoolbridge.v2.domain.academic.TodayCourse
-import kotlinx.coroutines.flow.Flow
-
-interface TodayScheduleRepository {
-    fun getTodayCourses(): Flow<List<TodayCourse>>
-}
