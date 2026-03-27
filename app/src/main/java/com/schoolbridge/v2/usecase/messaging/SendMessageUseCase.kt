2cea7bb52071e44e1657e9f4ea06be1a0be13616
oreste <muhirwa.g.oreste@gmail.com>
2026-03-27 11:25:38 +0100

    Implement `SendMessageUseCase` with repository integration and message mapping.

--- app/src/main/java/com/schoolbridge/v2/usecase/messaging/SendMessageUseCase.kt
+++ app/src/main/java/com/schoolbridge/v2/usecase/messaging/SendMessageUseCase.kt
@@ -1,4 +1,16 @@
 package com.schoolbridge.v2.usecase.messaging
 
-class SendMessageUseCase {
+import com.schoolbridge.v2.data.repository.interfaces.MessagingRepository
+import com.schoolbridge.v2.domain.messaging.Message
+import com.schoolbridge.v2.mapper.messaging.toDomain
+import com.schoolbridge.v2.mapper.messaging.toDto
+
+class SendMessageUseCase(
+    private val messagingRepository: MessagingRepository
+) {
+    suspend operator fun invoke(threadId: String, message: Message): Message {
+        return messagingRepository
+            .sendMessage(threadId = threadId, message = message.toDto())
+            .toDomain()
+    }
 }
