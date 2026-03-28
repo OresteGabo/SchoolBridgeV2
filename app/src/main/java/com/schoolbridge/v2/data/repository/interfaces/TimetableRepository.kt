45f64d059d2255e588ae3e04fd38e4ff2938dc8e
oreste <muhirwa.g.oreste@gmail.com>
2026-03-28 22:01:26 +0100

    feat: Add `createPersonalPlan` method to `TimetableRepository` interface
    Expanded `TimetableRepository` with a `createPersonalPlan` method to support creating personalized timetable plans using `CreatePersonalTimetablePlanRequestDto` and `MobilePersonalTimetablePlanDto`.

--- app/src/main/java/com/schoolbridge/v2/data/repository/interfaces/TimetableRepository.kt
+++ app/src/main/java/com/schoolbridge/v2/data/repository/interfaces/TimetableRepository.kt
@@ -1,7 +1,10 @@
 package com.schoolbridge.v2.data.repository.interfaces
 
+import com.schoolbridge.v2.data.dto.academic.CreatePersonalTimetablePlanRequestDto
+import com.schoolbridge.v2.data.dto.academic.MobilePersonalTimetablePlanDto
 import com.schoolbridge.v2.data.dto.academic.MobileTimetableResponseDto
 
 interface TimetableRepository {
     suspend fun getTimetable(): MobileTimetableResponseDto
+    suspend fun createPersonalPlan(request: CreatePersonalTimetablePlanRequestDto): MobilePersonalTimetablePlanDto
 }
