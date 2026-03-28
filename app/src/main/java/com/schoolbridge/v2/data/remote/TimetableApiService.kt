d7edc95e57a31880531de60e14ac751e624f8ef7
oreste <muhirwa.g.oreste@gmail.com>
2026-03-28 22:00:58 +0100

    feat: Add `createPersonalPlan` API integration to `TimetableApiService`
    Introduced the `createPersonalPlan` method in `TimetableApiService` to support creating personalized timetable plans, utilizing the new `CreatePersonalTimetablePlanRequestDto` and `MobilePersonalTimetablePlanDto`.

--- app/src/main/java/com/schoolbridge/v2/data/remote/TimetableApiService.kt
+++ app/src/main/java/com/schoolbridge/v2/data/remote/TimetableApiService.kt
@@ -1,5 +1,7 @@
 package com.schoolbridge.v2.data.remote
 
+import com.schoolbridge.v2.data.dto.academic.CreatePersonalTimetablePlanRequestDto
+import com.schoolbridge.v2.data.dto.academic.MobilePersonalTimetablePlanDto
 import com.schoolbridge.v2.data.dto.academic.MobileTimetableResponseDto
 import com.schoolbridge.v2.data.session.UserSessionManager
 import io.ktor.client.HttpClient
@@ -8,13 +10,17 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 interface TimetableApiService {
     suspend fun getTimetable(): MobileTimetableResponseDto
+    suspend fun createPersonalPlan(request: CreatePersonalTimetablePlanRequestDto): MobilePersonalTimetablePlanDto
 }
 
 class TimetableApiServiceImpl(
@@ -46,6 +52,24 @@ class TimetableApiServiceImpl(
             client.get("$BASE_URL/mobile/timetable") {
                 accept(ContentType.Application.Json)
                 header(HttpHeaders.Authorization, "Bearer $token")
+            }.body()
+        }
+    }
+
+    override suspend fun createPersonalPlan(request: CreatePersonalTimetablePlanRequestDto): MobilePersonalTimetablePlanDto {
+        val token = userSessionManager.getAuthToken()
+            ?: throw IllegalStateException("Missing auth token for timetable request")
+
+        return runApiCall(
+            defaultMessage = "Could not create your personal plan.",
+            authFailureStatusCodes = authFailureStatusCodes,
+            onAuthFailure = { userSessionManager.clearSession() }
+        ) {
+            client.post("$BASE_URL/mobile/timetable/personal-plans") {
+                accept(ContentType.Application.Json)
+                contentType(ContentType.Application.Json)
+                header(HttpHeaders.Authorization, "Bearer $token")
+                setBody(request)
             }.body()
         }
     }
