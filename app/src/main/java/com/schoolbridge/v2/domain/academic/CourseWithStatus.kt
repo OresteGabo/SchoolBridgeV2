9d43985097103ec03ce431339adfc5fc40ae9993
oreste <muhirwa.g.oreste@gmail.com>
2026-03-28 06:30:27 +0100

    Refactor: Remove CourseWithStatus domain class
    This commit deletes the `CourseWithStatus` data class from the `com.schoolbridge.v2.domain.academic` package. This class, which previously paired a `Course` object with a `CourseStatus`, is no longer required.

--- app/src/main/java/com/schoolbridge/v2/domain/academic/CourseWithStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.schoolbridge.v2.domain.academic
-
-// Extend your Course with a dummy flag for finished + validated (since not in your class)
-data class CourseWithStatus(
-    val course: Course,
-    val status: CourseStatus
-)
