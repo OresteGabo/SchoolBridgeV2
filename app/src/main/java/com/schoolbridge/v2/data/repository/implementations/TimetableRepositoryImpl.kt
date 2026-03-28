0e273f7981f2defbeb71f7cedfc15b1f99bba338
oreste <muhirwa.g.oreste@gmail.com>
2026-03-28 22:01:34 +0100

    feat: Implement `createPersonalPlan` in `TimetableRepositoryImpl` for API integration

--- app/src/main/java/com/schoolbridge/v2/data/repository/implementations/TimetableRepositoryImpl.kt
+++ app/src/main/java/com/schoolbridge/v2/data/repository/implementations/TimetableRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.schoolbridge.v2.data.repository.implementations
 
+import com.schoolbridge.v2.data.dto.academic.CreatePersonalTimetablePlanRequestDto
+import com.schoolbridge.v2.data.dto.academic.MobilePersonalTimetablePlanDto
 import com.schoolbridge.v2.data.dto.academic.MobileTimetableResponseDto
 import com.schoolbridge.v2.data.remote.TimetableApiService
 import com.schoolbridge.v2.data.repository.interfaces.TimetableRepository
@@ -8,4 +10,7 @@ class TimetableRepositoryImpl(
     private val timetableApiService: TimetableApiService
 ) : TimetableRepository {
     override suspend fun getTimetable(): MobileTimetableResponseDto = timetableApiService.getTimetable()
+
+    override suspend fun createPersonalPlan(request: CreatePersonalTimetablePlanRequestDto): MobilePersonalTimetablePlanDto =
+        timetableApiService.createPersonalPlan(request)
 }
